ed72af5b833a60a4d74bb889ebb6cb2f47851055
Zeeshan Ali <zapt1860@gmail.com>
2018-04-28 23:57:07 +0500

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+# A palindrome is a string that when revered turns out to be the same.
+# For example; aba, babab, aa, zeez, eve and adada are all palindromes.
+
+def isPalindrome(s): # Complxty O(n/2)
+    '''
+    INPUT:
+        s: string
+    OUTPUT:
+        True: if s is a palindrome
+        False: if s is not a palindrome
+    '''
+    for i in range(len(s)//2):
+        if s[i] is not s[-1-i]:
+            return False
+        print (s[i], "is", s[-1-i])
+    return True
